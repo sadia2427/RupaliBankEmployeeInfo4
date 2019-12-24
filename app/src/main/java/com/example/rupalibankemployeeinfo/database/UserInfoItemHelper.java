461f2e9a99c0d8a33dcb8580ef528e6c8a18e547
irensultanapony <ponyirenpony@gmail.com>
2019-12-24 16:40:46 +0600

    ADD DATABASE

---
+++ app/src/main/java/com/example/rupalibankemployeeinfo/database/UserInfoItemHelper.java
@@ -0,0 +1,26 @@
+package com.example.rupalibankemployeeinfo.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UserInfoItemHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="USER_DATABASE";
+    public static final int DATABASE_VERSION= 1;
+
+    public UserInfoItemHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/rupalibankemployeeinfo/database/UserInfoManager.java
@@ -0,0 +1,26 @@
+package com.example.rupalibankemployeeinfo.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class UserInfoManager {
+    private SQLiteDatabase sqLiteDatabase;
+    private UserInfoItemHelper userInfoItemHelper;
+
+    public UserInfoManager(Context context){
+        userInfoItemHelper=new UserInfoItemHelper(context);
+    }
+
+    private void open(){
+        sqLiteDatabase=userInfoItemHelper.getWritableDatabase();
+    }
+    private void close(){
+        userInfoItemHelper.close();
+    }
+    public boolean insertItem(UserInfoModel userInfoModel){
+        this.open();
+        ContentValues cv=new ContentValues();
+        return true;
+    }
+}
